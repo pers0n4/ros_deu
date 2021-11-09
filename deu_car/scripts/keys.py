79ad83a8835a0e5d90a297b70b4a54f896ae7111
Dong-Young Kim <31337.persona@gmail.com>
2021-11-10 00:18:35 +0900

    feat: add `keys` script for select lane by keystrokes
    ISSUE close #1

---
+++ deu_car/scripts/keys.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import select
+import sys
+import termios
+import tty
+
+import rospy
+from gazebo_msgs.msg import ModelState
+from gazebo_msgs.srv import SetModelState
+
+key_mapping = {
+    "1": {"x": -6.25, "y": 10.5, "z": 0},
+    "2": {"x": -7.4, "y": 10.5, "z": 0},
+}
+
+if __name__ == "__main__":
+    rospy.init_node("keyboard_driver")
+    rospy.wait_for_service("/gazebo/set_model_state")
+    try:
+        set_state = rospy.ServiceProxy("/gazebo/set_model_state", SetModelState)
+
+        old_attr = termios.tcgetattr(sys.stdin)
+        tty.setcbreak(sys.stdin.fileno())
+
+        print("Publishing keystrokes. Press Ctrl-C to exit...")
+
+        # TODO: 키보드 입력을 통한 위치 변경은 출발 이전에만 가능함
+        while not rospy.is_shutdown():
+            if select.select([sys.stdin], [], [], 0)[0] == [sys.stdin]:
+                key = sys.stdin.read(1)
+                values = key_mapping[key]
+
+                state_msg = ModelState()
+                state_msg.model_name = "mobile_base"
+
+                state_msg.pose.position.x = values["x"]
+                state_msg.pose.position.y = values["y"]
+                state_msg.pose.position.z = values["z"]
+
+                set_state(state_msg)
+
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_attr)
+    except rospy.ServiceException:
+        print("Service call failed")
